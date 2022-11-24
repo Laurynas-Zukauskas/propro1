61a46051dc9909d7c862afe68148ed37395e122d
Laurynas-Zukauskas <118725516+Laurynas-Zukauskas@users.noreply.github.com>
2022-11-24 17:42:35 +0200

    Add files via upload

---
+++ 1.2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    printf("Laurynas\nZukauskas\nProgramu Sistemos 1 kursas\n3 grupe\n");
+    printf("\n   __   ___  __  __\n  / /  / _ |/ / / /\n / /__/ __ / /_/ / \n/____/_/ |_\\____/");
+    return 0;
+}
